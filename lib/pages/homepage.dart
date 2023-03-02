e9d65898f2fdef41b95693514282d26c6cd7dfdd
Anish Subedi <anishsubedi599@gmail.com>
2023-03-02 22:48:08 +0545

    i have done it

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -2,8 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:cupertino_icons/cupertino_icons.dart';
 import 'package:endproject/main.dart';
-
-
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 
 class homepage extends StatefulWidget {
@@ -55,17 +54,43 @@ home: Scaffold(
   appBar: AppBar(
     title: Text('STONKWATCH'),
 actions: [
-  Icon(Icons.settings)
+  Icon(Icons.person ),
+  
 ],
 
 
   ),
-
+bottomNavigationBar: GNav(
+  backgroundColor: Color.fromARGB(255, 15, 15, 16),
+  color: Color.fromARGB(255, 243, 240, 241),
+  activeColor: Colors.white,
+  tabBackgroundColor: Colors.grey.shade800,
+  gap: 8,
+  padding: EdgeInsets.all(16),
+  tabs: [
+GButton(icon: Icons.home,
+text: 'Home',
+),
+GButton(icon: Icons.list,
+text: 'List',
+),
+GButton(icon: Icons.search,
+text: 'Search',
+),
+GButton(icon: Icons.settings,
+text: 'setting',
 ),
 
+  ],
+),
+
+  
+),
+);
 
 
 
-    );
+
+    
   }
 }
